12079b8ebc5336d8935e62bff93c07c00cebae1a
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-04 00:32:34 +0900

    :alembic: Endpointでの自前のPlugの使い方 plugマクロで追加するだけ。 上から順に流れるので意図した動作になるよう追加する。 EndpointのPlugは全てのリクエストに影響を与える。

--- dataflow_Phoenix/lib/dataflow_Phoenix_web/endpoint.ex
+++ dataflow_Phoenix/lib/dataflow_Phoenix_web/endpoint.ex
@@ -45,5 +45,6 @@ defmodule Dataflow_PhoenixWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug Dataflow_PhoenixWeb.Plugs.DoNothing
   plug Dataflow_PhoenixWeb.Router
 end
