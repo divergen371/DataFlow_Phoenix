c8d76fbf3d2034b9cfe2b3a733a82f78ddf12dfe
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-04 11:40:57 +0900

    :alembic: レスポンス送信後に再度レスポンスを送信しようとしたためエラー発生 OnlyAdminPlugでリダイレクトしたあとでindexアクションが作動しているのが原因。 Plugはコントローラのアクションまでデータフローを継続させるのでやりたいことによってはその流れを中断させる必要がある

---
+++ dataflow_Phoenix/lib/dataflow_Phoenix_web/auth/only_admin.ex
@@ -0,0 +1,19 @@
+defmodule Dataflow_PhoenixWeb.Auth.OnlyAdmin do
+  import Phoenix.Controller, only: [redirect: 2]
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    if admin?(conn) do
+      conn
+    else
+      conn
+      |> redirect(external: "https://www.phoenixframework.org/")
+    end
+  end
+
+  defp admin?(_conn) do
+    false
+  end
+end
--- dataflow_Phoenix/lib/dataflow_Phoenix_web/controllers/page_controller.ex
+++ dataflow_Phoenix/lib/dataflow_Phoenix_web/controllers/page_controller.ex
@@ -1,12 +1,10 @@
 defmodule Dataflow_PhoenixWeb.PageController do
   use Dataflow_PhoenixWeb, :controller
-  plug Dataflow_PhoenixWeb.Plugs.DoNothing
-  plug :do_nothing_plug
+
+  plug(Dataflow_PhoenixWeb.Auth.OnlyAdmin)
 
   def index(conn, _params) do
+    IO.puts("Only administrator can access.")
     render(conn, "index.html")
-    |> IO.inspect()
   end
-
-  defp do_nothing_plug(conn, _opts), do: conn
 end
