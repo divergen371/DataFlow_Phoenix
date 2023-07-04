be783dee27e021ff6b5f90e7f496606e1c7731d0
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-04 11:17:19 +0900

    :alembic: コントローラでPlugの呼び出し ローカルでPlug関数を定義して通すこともできる

--- dataflow_Phoenix/lib/dataflow_Phoenix_web/controllers/page_controller.ex
+++ dataflow_Phoenix/lib/dataflow_Phoenix_web/controllers/page_controller.ex
@@ -1,8 +1,12 @@
 defmodule Dataflow_PhoenixWeb.PageController do
   use Dataflow_PhoenixWeb, :controller
+  plug Dataflow_PhoenixWeb.Plugs.DoNothing
+  plug :do_nothing_plug
 
   def index(conn, _params) do
     render(conn, "index.html")
     |> IO.inspect()
   end
+
+  defp do_nothing_plug(conn, _opts), do: conn
 end
